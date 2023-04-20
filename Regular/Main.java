4dd73d0baa3c244e91c3fb4c35bb87479788c03d
Ekats3529 <45201820+Ekats3529@users.noreply.github.com>
2023-04-20 23:02:37 +0400

    Create Main.java

---
+++ Regular/Main.java
@@ -0,0 +1,36 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        Pattern pattern1 = Pattern.compile("<img\\s+(.*)src\\s*=\\s*(.*)");
+
+        File file = new File("src/input.txt");
+
+        int k = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                k += 1;
+                Matcher matcher1 = pattern1.matcher(line);
+                if (matcher1.find()){
+                    System.out.println(Integer.toString(k) + ". " +line);
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
